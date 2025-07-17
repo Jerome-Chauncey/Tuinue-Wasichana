05ecaec3e9e5210081993895879ae340e450fb2c
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-07-17 10:35:47 +0300

    Allow render hostname in Vite preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // allow Vite Preview to listen on all interfaces
+  preview: {
+    host: '0.0.0.0',
+    // add your Render-assigned hostname here:
+    allowedHosts: [
+      'tuinue-wasichana-ui-dw85.onrender.com'
+    ]
+  }
 })
