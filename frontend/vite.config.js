fbf5b09bcd2e0c72b21ce3009633eae6736ba241
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-07-18 20:52:01 +0300

    render API fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000", // Adjust to your backend URL when ready
-    },
-  },
-});
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'tuinue-wasichana-ui-dw85.onrender.com'
+    ]
+  }
+})
